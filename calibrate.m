84a14227b7b75658bde4b29b129dd1dc5bee716b
Conrad Tulig <48301800+cctulig@users.noreply.github.com>
2020-01-28 18:43:44 -0500

    Adds descriptive comments for the calibrate method

--- calibrate.m
+++ calibrate.m
@@ -1,24 +1,28 @@
 function [ output_args ] = calibrate( pp )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%calibrate Calibrations the home position of the arm
+%   When this function is called, ask the status server for the current motor positions,
+%   assign them to a new reset packet, and send the reset to the Calibration server
+%   to assign the arm's home position to the robot's current position.
 
 for i = 1 : 3
-            pause(1);
+    pause(1); %3 loops with pauses to make sure calibration is sent
 
+    %Initialize packets to be sent/recieved from servers
     returnPacket = zeros(15, 1, 'single');
     resetPacket = zeros(15, 1, 'single');
     
-    
-    display('I pushed the pushbutton')
+    %Get current encoder values of the arm from the STATUS server
     pp.write(03, returnPacket);
     pause(0.003);
     returnPacket = pp.read(03);
     disp(returnPacket);
     
+    %Extract the encoder values from the status packet and put them into the reset packet
     resetPacket(1) = returnPacket(1);
     resetPacket(2) = returnPacket(4);
     resetPacket(3) = returnPacket(7);
     
+    %Send the reset packet to the Calibration server
     pp.write(04, resetPacket);
     end
 end
