ebb40634d2a86c96efab9d9cd01759ab4b337f12
cctulig <cctulig@gmail.com>
2020-01-31 17:54:09 -0500

    Graphs the angles, positions, and velocities of the arm

--- src/lab2.m
+++ src/lab2.m
@@ -42,18 +42,24 @@ CALIBRATION_ID = 04;
 pp = PacketProcessor(myHIDSimplePacketComs);
 disp('START!')
 
+n=500;
 statusMatrix = zeros(6, 7);
 row = 1;
 empty = zeros(15, 1, 'single');
+positionMatrix=zeros(n, 7);
+i=1;
 
+ xPos = [55, 41, 650];
+ zPos = [-290, 300, -350];
 
 try
     
     
     DEBUG   = true;          % enables/disables debug prints
     tic
-    while toc < 60
-       
+    
+    while i < n
+           
           %Write and reading from the Status server to get encoder positions and motor velocities
             pp.write(STATUS_ID, empty);
             
@@ -63,11 +69,51 @@ try
             statusPacket = pp.read(STATUS_ID);
             
             angle = [(statusPacket(1)*2*pi/4096), (statusPacket(4)*2*pi/4096), (statusPacket(7)*2*pi/4096)];
-            
+            disp(angle)
             stickModel(angle)
-        
+            position = fwkin3001(angle(1),angle(2),angle(3));
+            matrix = [angle(1),angle(2),angle(3),position(1),position(2),position(3)];
+            positionMatrix(i,1)= angle(1);
+            positionMatrix(i,2)= angle(2);
+            positionMatrix(i,3) = angle(3);
+            positionMatrix(i,4) = position(1);
+            positionMatrix(i,5) = position(2);
+            positionMatrix(i,6) = position(3);
+            positionMatrix(i,7) = toc;
+            i=i+1;
     end
+    
+    csvwrite('positionMatrix.csv',positionMatrix);
+    
+    figure(2)
+    plot(positionMatrix(:,7),positionMatrix(:,1),'r')
+    hold on
+    plot(positionMatrix(:,7),positionMatrix(:,2),'b')
+    plot(positionMatrix(:,7),positionMatrix(:,3),'g')
+    title('Joint Angles');
+    set(gca, 'fontsize', 16);
+    legend({'Theta1', 'Theta2', 'Theta3'});
 
+    hold off
+    
+    figure(3)
+    plot(positionMatrix(:,7),positionMatrix(:,4),'r')
+    hold on
+    plot(positionMatrix(:,7),positionMatrix(:,6),'b')
+    title('End Effector');
+    set(gca, 'fontsize', 16);
+    legend({'X Pos', 'Z Pos'});
+    hold off
+    
+    figure(4)
+    plot(positionMatrix(1:end-1,7),rdivide(diff(positionMatrix(:,4)'),diff(positionMatrix(:,7)')) ,'r')
+    hold on
+    plot(positionMatrix(:,7),rdivide(diff(positionMatrix(:,6)'),diff(positionMatrix(:,7)')),'b')
+    title('Velocity');
+    set(gca, 'fontsize', 16);
+    legend({'X Vel', 'Z Vel'});
+    hold off
+    
 catch exception
     getReport(exception)
     disp('Exited on error, clean shutdown');
@@ -78,7 +124,7 @@ end
 pp.shutdown()
 
 function [] = stickModel(q)
-%UNTITLED Summary of this function goes here
+%UNTITLED Summa,ry of this function goes here
 %   Detailed explanation goes here
 
 link1 = [cos(q(1)), 0, sin(q(1)), 0;
@@ -99,7 +145,7 @@ v2 = link1*link2;
 v2 = v2(1:3,4)';
 v3 = link1*link2*link3;
 v3 = v3(1:3,4);
-
+figure(1)
 plot3([0,v1(1),v2(1),v3(1)],[0,v1(2),v2(2), v3(2)],[0,v1(3),v2(3), v3(3)],'b-o')
 hold on
 xlim([0 400]), ylim([-400 400]), zlim([0 400]);
