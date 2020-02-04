a2fa8995df1a8e8e35ed3c2369c8c83760141c8c
renthcifok <kofichtner@wpi.edu>
2020-02-03 19:47:41 -0500

    Implements cubic trajectory with 40 interpolated setpoints

--- src/lab2.m
+++ src/lab2.m
@@ -47,21 +47,48 @@ empty = zeros(15, 1, 'single');
 positionMatrix=zeros(3, 7);
 i=1;
 
-joint2 = [55, 41, 650];
-joint3 = [-290, 300, -350];
+
+traj2A = cubicTrajectory(0,rad(55),0,1,0,0);
+traj2B = cubicTrajectory(rad(55),rad(41),1,2,0,0);
+traj2C= cubicTrajectory(rad(41),rad(650),2,3,0,0);
+traj2D= cubicTrajectory(rad(650),rad(55),3,4,0,0);
+
+traj3A = cubicTrajectory(0,rad(-290),0,1,0,0);
+traj3B = cubicTrajectory(rad(-290),rad(300),1,2,0,0);
+traj3C = cubicTrajectory(rad(300),rad(-350),2,3,0,0);
+traj3D = cubicTrajectory(rad(-350),rad(-290),3,4,0,0);
+
+[interp2A, time2A] = interpolate(traj2A, 0,1);
+[interp2B, time2B] = interpolate(traj2B, 1,2);
+[interp2C, time2C] = interpolate(traj2C, 2,3);
+[interp2D, time2D] = interpolate(traj2D, 3,4);
+
+[interp3A, ] = interpolate(traj3A, 0,1);
+[interp3B, ] = interpolate(traj3B, 1,2);
+[interp3C, ] = interpolate(traj3C, 2,3);
+[interp3D, ] = interpolate(traj3D, 3,4);
+
+trajJoint2 = [interp2A interp2B interp2C interp2D];
+trajJoint3 = [interp3A interp3B interp3C interp3D];
+time = [time2A time2B time2C time2D];
+
+joint2 = [55, 41, 650, 55];
+joint3 = [-290, 300, -350, -290];
 packet = zeros(15, 1, 'single');
 index = 1;
 notReachedSetpoint = 1;
 
 
+
 try
     tic
-    for k = 1:1
+    for k = 1:4
+        
         DEBUG   = true;          % enables/disables debug prints
         
         
-        packet(4) = joint2(index); %Writes setpoint to joint 2
-        packet(7) = joint3(index); %Writes setpoint to joint 3
+        packet(4) = joint2(k); %Writes setpoint to joint 2
+        packet(7) = joint3(k); %Writes setpoint to joint 3
         
         % Send packet to the server and get the response
         %pp.write sends a 15 float packet to the micro controller
@@ -72,7 +99,7 @@ try
         %pp.read reads a returned 15 float backet from the nucleo.
         returnPacket = pp.read(SERV_ID);
         notReachedSetpoint = 1;
-        targetTime = toc + 2;
+        targetTime = toc + 1;
         while notReachedSetpoint
             
             %Write and reading from the Status server to get encoder positions and motor velocities
@@ -95,14 +122,20 @@ try
             positionMatrix(i,5) = position(2);
             positionMatrix(i,6) = position(3);
             positionMatrix(i,7) = toc;
-            i=i+1;
             
-             
+            
             %Once setpoint is roughly reached, move on to the next setpoint
+%             if statusPacket(4) < joint2(k) + 10 && statusPacket(4) > joint2(k) - 10
+%                 if statusPacket(7) < joint3(k) + 10 && statusPacket(7) > joint3(k) - 10
+%                     notReachedSetpoint = 0;
+%                 end
+%             end
+
             if targetTime < toc
                 notReachedSetpoint = 0;
-                index = index + 1;
             end
+            
+            i=i+1;
         end
     end
     
@@ -113,38 +146,60 @@ try
     
     figure(2)
     plot(positionMatrix(:,7),positionMatrix(:,1),'r', 'LineWidth', 2)
+    grid on
     hold on
     plot(positionMatrix(:,7),positionMatrix(:,2),'b', 'LineWidth', 2)
     plot(positionMatrix(:,7),positionMatrix(:,3),'g', 'LineWidth', 2)
-    title('Joint Angles');
+    title('Joint Angle vs. Time');
     set(gca, 'fontsize', 16);
     legend({'Theta1', 'Theta2', 'Theta3'});
-    
-    
+    xlabel('Time[s]'), ylabel('Angle[rad]');
     hold off
     
     figure(3)
     plot(positionMatrix(:,7),positionMatrix(:,4),'r', 'LineWidth', 2)
+    grid on
     hold on
     plot(positionMatrix(:,7),positionMatrix(:,6),'b', 'LineWidth', 2)
-    title('End Effector');
+    title('End Effector Position vs. Time');
     set(gca, 'fontsize', 16);
     legend({'X Pos', 'Z Pos'});
+    xlabel('Time[s]'), ylabel('Position[mm]');
     hold off
     
     figure(4)
-    plot(positionMatrix(1:end-1,7),rdivide(diff(positionMatrix(:,4)'),diff(positionMatrix(:,7)')) ,'r', 'LineWidth', 2)
+    plot(positionMatrix(1:end-1,7),rdivide(diff(positionMatrix(:,1)'),diff(positionMatrix(:,7)')) ,'r', 'LineWidth', 2)
+    grid on
     hold on
-    plot(positionMatrix(1:end-1,7),rdivide(diff(positionMatrix(:,6)'),diff(positionMatrix(:,7)')),'b', 'LineWidth', 2)
-    title('Velocity');
+    plot(positionMatrix(1:end-1,7),rdivide(diff(positionMatrix(:,2)'),diff(positionMatrix(:,7)')),'b', 'LineWidth', 2)
+    plot(positionMatrix(1:end-1,7),rdivide(diff(positionMatrix(:,3)'),diff(positionMatrix(:,7)')),'g', 'LineWidth',2)
+    title('Joint Velocity vs. Time');
     set(gca, 'fontsize', 16);
-    legend({'X Vel', 'Z Vel'});
+    legend({'Joint 1', 'Joint 2','Joint 3'});
+    xlabel('Time[s]'), ylabel('Velocity[rad/s]');
     hold off
     
-    disp('Traj:')
-    traj1 = cubicTrajectory(0,angle(1),0,1,0,0)
-    traj2 = cubicTrajectory(0,angle(2),0,1,0,0)
-    traj3 = cubicTrajectory(0,angle(3),0,1,0,0)
+    figure(5)
+    plot(positionMatrix(:,4),positionMatrix(:,6),'r', 'LineWidth', 2)
+    grid on
+    hold on
+    title('X-Position vs. Z-Position ');
+    set(gca, 'fontsize', 16);
+    xlabel('X Position[mm]'), ylabel('Z Position[mm]');
+    hold off
+    
+    
+    figure(6)
+    plot(time,zeros(1,40),'r', 'LineWidth', 2)
+    grid on
+    hold on
+    plot(time,trajJoint2,'b', 'LineWidth', 2)
+    plot(time,trajJoint3,'g', 'LineWidth', 2)
+    title('Planned Trajectory');
+    set(gca, 'fontsize', 16);
+    legend({'Theta1', 'Theta2', 'Theta3'});
+    xlabel('Time[s]'), ylabel('Angle[rad]');
+    hold off
     
 catch exception
     getReport(exception)
@@ -185,12 +240,39 @@ hold off
 
 end
 
-function [sol] = cubicTrajectory(qi,qf,ti,tf,vi,vf)
+function [a] = cubicTrajectory(qi,qf,ti,tf,vi,vf)
 syms a0 a1 a2 a3;
 
 eqns = [a0 + a1*ti + a2*ti^2 + a3*ti^3 == qi,
     a1 + 2*a2*ti + 3*a3*ti^2 == vi,
     a0 + a1*tf + a2*tf^2 + a3*tf^3 == qf,
     a1 + 2*a2*tf + 3*a3*tf^2 == vf];
-sol = fsolve(eqns,[a0 a1 a2 a3]);
+
+out = solve(eqns,[a0 a1 a2 a3]);  %'MaxDegree', 3);
+
+a(1)=out.a0;
+a(2)=out.a1;
+a(3)=out.a2;
+a(4)=out.a3;
 end
+
+function [q,time] = interpolate(a,ti,tf)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+step = (tf-ti)/10;
+n=1;
+val= zeros(1,10);
+timeStamp = zeros(1,10);
+for t = ti+step:step:tf
+    val(1,n)= a(1)+a(2)*t+a(3)*t^2+a(4)*t^3;
+    timeStamp(1,n)= t;
+    n=n+1;
+end
+time = timeStamp;
+q = val; %(4096/(2*pi))*val;
+end
+
+function [angle] = rad(enc)
+angle=enc*((2*pi)/4096);
+end
+
