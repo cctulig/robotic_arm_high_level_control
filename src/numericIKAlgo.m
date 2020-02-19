17accc1311a3acaadfe0aaf511cd892d429a616f
Conrad Tulig <48301800+cctulig@users.noreply.github.com>
2020-02-18 22:43:05 -0500

    Adds comments to numericIKAlgo.m function

--- src/numericIKAlgo.m
+++ src/numericIKAlgo.m
@@ -1,20 +1,20 @@
 function [ dq ] = numericIKAlgo( qi, Pf )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%numericIKAlgo Inverse Differential Kinematics Calculations
 
+% Gain factor
 G = eye(3,3).*[.1;.1;.1];
+
+% Jacobian/Inverse Position Jacobian
 J = jacob0(qi);
 Jp = J(1:3,1:3);
 Jpi = pinv(Jp);
 
+% Change in position
 Pi = fwkin3001(qi(1), qi(2), qi(3));
 Pdelta = (Pf' - Pi);
 
+% Calculate change in angle
 dq = (Jpi * G * Pdelta)';
-%ikin w/ jacobian -> get positon, get jacobian (3,3) invert, multiply by
-%deriv of final pos and ^ pos
-%
-
 
 end
 
